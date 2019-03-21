bf71816692bcc5d0f7fdd4ef1796dcbb21a4dda5
Huseyin Simsek <huseyin.simsek@boun.edu.tr>
2019-03-21 11:11:08 +0300

    viewcomponent start

---
+++ CetBlog/Views/Shared/Components/CategoryMenu/CategoryMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using CetBlog.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBlog.Views.Shared.Components.CategoryMenu
+{
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext dbContext;
+            
+
+        public CategoryMenuViewComponent(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
